2eecf4019012fba5ea2bf2435fcf73bbbe8f340a
Alin Florin <alin@qoffice.ro>
2024-10-03 18:55:51 +0300

    -

--- services/db.service.web.ts
+++
@@ -1,9 +0,0 @@
-class DbService {
-  async init() {
-    console.log('DB Init not needed');
-    await Promise.resolve();
-  }
-}
-
-export const dbService = new DbService();
-export default dbService;
