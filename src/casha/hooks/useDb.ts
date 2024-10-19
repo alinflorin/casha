1ad9abf9205886f52304b0dc9b384230ae626da9
Alin Florin <alin@qoffice.ro>
2024-10-19 10:27:56 +0300

    -

--- src/casha/hooks/useDb.ts
+++ src/casha/hooks/useDb.ts
@@ -4,7 +4,8 @@ import { useMemo } from "react";
 
 const initSql = `
 CREATE TABLE IF NOT EXISTS cars (
-	vin integer primary key NOT NULL UNIQUE,
+	id integer primary key NOT NULL UNIQUE,
+	vin TEXT NOT NULL UNIQUE,
 	display_name TEXT NOT NULL,
 	make TEXT NOT NULL,
 	model TEXT NOT NULL,
@@ -14,21 +15,40 @@ CREATE TABLE IF NOT EXISTS cars (
 );
 CREATE TABLE IF NOT EXISTS service_items (
 	id integer primary key NOT NULL UNIQUE,
-	name TEXT NOT NULL,
-	service_interval_months INTEGER,
-	service_interval_km INTEGER
-);
-CREATE TABLE IF NOT EXISTS active_service_items (
-	vin TEXT NOT NULL,
-	service_item_id INTEGER NOT NULL,
+	car_id INTEGER NOT NULL,
+	name INTEGER NOT NULL UNIQUE,
 	service_interval_months INTEGER,
 	service_interval_km INTEGER,
-FOREIGN KEY(vin) REFERENCES cars(vin),
-FOREIGN KEY(service_item_id) REFERENCES service_items(id)
+FOREIGN KEY(car_id) REFERENCES cars(id)
 );
 CREATE TABLE IF NOT EXISTS settings (
 	key TEXT NOT NULL UNIQUE,
 	value TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS shops (
+	id integer primary key NOT NULL UNIQUE,
+	name TEXT NOT NULL UNIQUE
+);
+CREATE TABLE IF NOT EXISTS service_visits (
+	id integer primary key NOT NULL UNIQUE,
+	car_id INTEGER NOT NULL,
+	shop_id INTEGER NOT NULL,
+	visit_date REAL NOT NULL,
+FOREIGN KEY(car_id) REFERENCES cars(id),
+FOREIGN KEY(shop_id) REFERENCES shops(id)
+);
+CREATE TABLE IF NOT EXISTS service_visits_service_items (
+	id integer primary key NOT NULL UNIQUE,
+	service_visit_id INTEGER NOT NULL,
+	service_item_id INTEGER NOT NULL,
+FOREIGN KEY(service_visit_id) REFERENCES service_visits(id),
+FOREIGN KEY(service_item_id) REFERENCES service_items(id)
+);
+CREATE TABLE IF NOT EXISTS common_service_items (
+	id integer primary key NOT NULL UNIQUE,
+	name TEXT NOT NULL UNIQUE,
+	service_interval_months INTEGER,
+	service_interval_km INTEGER
 );
 `;
 
