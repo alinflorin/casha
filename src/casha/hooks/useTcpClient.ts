d4d701698ef9e0129cc4de85a9c022fa18fc27e3
Alin Florin <alin@qoffice.ro>
2024-12-02 02:03:34 +0200

    sdf

--- src/casha/hooks/useTcpClient.ts
+++ src/casha/hooks/useTcpClient.ts
@@ -12,9 +12,7 @@ export default function useTcpClient() {
             port: port,
             host: address
           },
-          () => {
-            console.log("COnnected");
-          }
+          () => {}
         );
         setClient(c);
 
